1659526ce51fe48fe15c5e0c272e99815a19ac9f
stilldisturbing <stilldisturbing@users.noreply.github.com>
2024-04-17 21:58:39 -0400

    Test Repository Initial Commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# Lennox iComfort WiFi Component
+A custom component for Home Assistant to integrate with Lennox iComfort WiFi thermostats and AirEase Comfort Sync thermostats.
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Default-orange.svg)](https://github.com/custom-components/hacs)
+
+> **Please Note:** This component **does not** support the Lennox iComfort S30 or Lennox iComfort E30 thermostats. For those thermostats please check out [PeteRager's repository](https://github.com/PeteRager/lennoxs30).
+
+# Requirements
+
+- Home Assistant >= 2022.12
+- Thermostat linked to a myicomfort.com (Lennox) or mycomfortsync.com (AirEase) account
+
+# Installation
+This integration is available in HACS for ease of installation.  
+If you wish to manually install this component, copy the 'myicomfort' folder and contents to &lt;HA config directory&gt;/custom_components/ 
+
+# Configuration
+### Example configuation
+```yaml
+climate:
+  - platform: myicomfort
+    name: firstfloor
+    username: !secret cloudapi_username
+    password: !secret cloudapi_password
+    system: 0
+    zone: 0
+    min_temp: 55
+    max_temp: 90
+    cloud_svc: airease
+```
+
+### Platform Parameters
+| Name | Type | Requirement | Default | Description |
+| ---- | ---- | ----------- | ------- | ----------- |
+| name | string | required | none | Entity name |
+| username | string | required | none | Cloud service account username |
+| password | string | required | none | Cloud service account password |
+| system | integer | optional | `0` | Select the system for integration if you have multiple on the account. |
+| zone | integer | optional | `0` | Select the zone for integration if the selected system has multiple zones. |
+| min_temp | integer | optional | `45` | Minimum temperature HA can set. |
+| max_temp | integer | optional | `95` | Maximum temperature HA can set. |
+| cloud_svc | string | optional | `lennox` | Cloud service selection - use `lennox` or `airease` | 
+
+### Multiple zones or systems
+Add additional entries under climate for each additional system or zone.
+```yaml
+climate:
+  - platform: myicomfort
+    name: Downstairs
+    username: !secret cloudapi_username
+    password: !secret cloudapi_password
+    system: 0 
+    zone: 0 
+    min_temp: 55
+    max_temp: 90
+    cloud_svc: lennox
+  - platform: myicomfort
+    name: ManCave
+    username: !secret cloudapi_username
+    password: !secret cloudapi_password
+    system: 0 
+    zone: 1 
+    min_temp: 45
+    max_temp: 75
+    cloud_svc: lennox
+  - platform: myicomfort
+    name: Upstairs
+    username: !secret cloudapi_username
+    password: !secret cloudapi_password
+    system: 1 
+    zone: 0 
+    min_temp: 65
+    max_temp: 80
+    cloud_svc: airease
+```
+
+# Notes
+
+### Humidity 
+The humidity is exposed to HA as an attribute of the climate entity but since the device doesn't support set_humdity, most of the thermostat cards won't display it. The template platform can be used to capture the humidity and make it accessible as a sensor. You can use the [Simple Thermostat card](https://github.com/nervetattoo/simple-thermostat) to add the sensor to a thermostat card.
+
+Below is a an example to expose the current humidity as a sensor. The climate device is named 'lennox' and the resulting sensor is 'lennox_humidity'.
+```yaml
+template:
+  - sensor:
+    - name: lennox_humidity
+      unit_of_measurement: '%'
+      state: '{{ states.climate.lennox.attributes.current_humidity | float }}'
+```
+
+### HA 0.96 to 2021.4 
+There were minor changes to the climate platform between 0.96 and 2021.4. You can read thru release notes in this repository to find a version that works for you.
+
+### HA 0.95 or older
+If for some reason you are still running HA 0.95 or older, you can still integrate with your thermostat. You just need to grab one of the older code sets from here: https://github.com/thevoltagesource/LennoxiComfort-archive
+
+# Credits
+My code is built on the work of Jerome Avondo (ut666)
+- API: https://github.com/ut666/LennoxPy
+- HA Component: https://github.com/ut666/Homeassistant/tree/master/custom_components/climate
+- Lennox Cloud API details: https://github.com/ut666/LennoxThermoPi-II
---
+++ custom_components/myicomfort/climate.py
@@ -0,0 +1,342 @@
+"""
+Lennox iComfort WiFi Climate Component for Home Assisant.
+
+By Jacob Southard (github.com/thevoltagesource)
+Based on the work of Jerome Avondo (github.com/ut666)
+
+Tested against Home Assistant Version: 2024.1.0
+
+Notes:
+  The away mode set points can only be set on the thermostat.  The code below
+  prevents changing set points when in away mode so there are no surprises
+  when leaving away mode.
+
+Issues:
+
+Ideas/Future:
+
+Change log:
+  20240103 - Changed const CURRENT_HVAC_* to use HVACAction.*
+             Changed const HVAC_MODE_* to use HVACMode.*
+             Changed const SUPPORT_* to use ClimateEntityFeature.*
+             Changed const TEMP_* to use UnitOfTemperature.*
+  20221130 - Create unique_id property which is generated as the seystem serial
+             number and the zone  <sn>z<zone>.
+  20211207 - Map api state = 3 (system waiting) as CURRENT_HVAC_IDLE. Add 
+             item system_waiting to extra_device_attributes as (boolean) True
+             when state = 3 otherwise False.
+  20211206 - Added SUPPORT_AUX_HEAT. Made required changes to integration to 
+             handle aux/emergency heat mode. API also required changes so the
+             required version was updated (>= v0.5.x).
+             also updated to support this mode.
+  20210609 - Bug Fix - Changed set_temperature logic to handle the way HomeKit
+             updates set points. Changed changed device_state_attributes to
+             extras_state_attributes to match climate platform change in 
+             HA 2021.4
+  20210507 - Update manifest.json file to add required items.
+  20200620 - Changed from ClimateDevice to CliemateEntity to support new
+             climate standard introduced in HA v0.110.
+  20200222 - Bump API version number.
+  20191222 - Bug fix - bump API version which addressed a zone selection bug.         
+  20191221 - Added support for AirEase thermostats.           
+  20190721 - Added ability to select cloud service (Lennox or AirEase).
+             Now verifies cloud API connection before adding entity to HA.
+             Requires myicomfort API v0.3.0 or higher
+  20190720 - Changed code to be HA Climate 1.0 compliant. The climate
+             integration was redesigned in Home Assistant 0.96.
+  20190505 - Moved requirements to manifest.json. Bumped API requirement to
+             myicomfort=0.2.1
+  20190314 - Changeed climate.const import to match HA change. Changed layout
+             to match new HA 0.88 architecture.
+  20190127 - Fixed items flagged by flask8 and pylint
+  20190125 - Switched reliance from local api file to PyPI hosted myicomfort
+             project. Moved import statement and added REQUIREMENTS variable
+             to match HA coding standards.
+  20190111 - Cleaned up code.
+  20181211 - Changed state() to return item from list of STATE_* constants so
+             the state will display in Lovelace. Removed manual entry of
+             current humidity in device attributes as 0.83 does this natively
+             now.
+  20181202 - Updated to work with changes made to API. Added configurable min
+             and max temp properties.
+  20181129 - Added TEMP_UNITS list and created property for temperature_unit to
+             report units used by tstat.
+  20181126 - Switched fan and op modes to report/accept HA STATE constants so
+             component meets current HA standards. This change fixes
+             compactibility with the Lovelace thermostate card. Cleaned up
+             notes/documentation.
+  20181125 - Cleaned up and simplified code. Using _api properties directly
+             instead of copying to other variables.
+  20181124 - Changed Away Mode response to fit current standards.
+  20180218 - Initial commit. Provides sensor data and allows control of all
+             manual modes.
+
+"""
+
+import logging
+
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.climate import ClimateEntity, PLATFORM_SCHEMA
+from homeassistant.components.climate.const import (
+    HVACAction,
+    HVACMode,
+    PRESET_AWAY,
+    PRESET_NONE,
+    ClimateEntityFeature,
+    FAN_ON,
+    FAN_AUTO,
+    ATTR_TARGET_TEMP_LOW,
+    ATTR_TARGET_TEMP_HIGH,
+)
+from homeassistant.const import (
+    CONF_USERNAME, CONF_PASSWORD, 
+    UnitOfTemperature, 
+    ATTR_TEMPERATURE)
+
+_LOGGER = logging.getLogger(__name__)
+
+# HA doesn't have a 'circulate' mode defined for fan.
+FAN_CIRCULATE = 'Circulate'
+
+SUPPORT_FLAGS = (ClimateEntityFeature.TARGET_TEMPERATURE |
+                 ClimateEntityFeature.TARGET_TEMPERATURE_RANGE |
+                 ClimateEntityFeature.PRESET_MODE |
+                 ClimateEntityFeature.FAN_MODE
+                )
+
+FAN_MODES = [
+    FAN_AUTO, FAN_ON, FAN_CIRCULATE
+]
+
+HVAC_MODES = [
+    HVACMode.OFF, HVACMode.HEAT, HVACMode.COOL, HVACMode.HEAT_COOL
+]
+
+HVAC_ACTIONS = [
+    HVACAction.IDLE, HVACAction.HEATING, HVACAction.COOLING
+]
+
+TEMP_UNITS = [
+    UnitOfTemperature.FAHRENHEIT, UnitOfTemperature.CELSIUS
+]
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Required('name'): cv.string,
+    vol.Optional('system', default=0): cv.positive_int,
+    vol.Optional('zone', default=0): cv.positive_int,
+    vol.Optional('min_temp'): vol.Coerce(float),
+    vol.Optional('max_temp'): vol.Coerce(float),
+    vol.Optional('cloud_svc', default='lennox'): cv.string
+})
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the climate platform."""
+    from myicomfort.api import Tstat
+
+    username = config.get(CONF_USERNAME)
+    password = config.get(CONF_PASSWORD)
+    system = config.get('system')
+    zone = config.get('zone')
+    name = config.get('name')
+    min_temp = config.get('min_temp')
+    max_temp = config.get('max_temp')
+    service = config.get('cloud_svc')
+
+    tstat = Tstat(username, password, system, zone, service)
+    climate = [LennoxClimate(name, min_temp, max_temp, tstat)]
+
+    if tstat.connected:
+        add_entities(climate, True)
+    else:
+        _LOGGER.error('Failed to connect to thermostat cloud API.')
+
+
+class LennoxClimate(ClimateEntity):
+    """Class for Lennox iComfort WiFi thermostat."""
+
+    def __init__(self, name, min_temp, max_temp, api):
+        """Initialize the climate device."""
+        self._name = name
+        self._api = api
+        self._min_temp = min_temp
+        self._max_temp = max_temp
+
+    def update(self):
+        """Update data from the thermostat API."""
+        self._api.pull_status()
+
+    @property
+    def extra_state_attributes(self):
+        """Return device specific state attributes."""
+        data = {}
+        data["system_waiting"] = True if self._api.state == 3 else False
+        return data
+
+    @property
+    def name(self):
+        """Return the name of the climate device."""
+        return self._name
+
+    @property
+    def unique_id(self):
+        """Return unique_id for this system/zone"""
+        return self._api.serial_number + 'z' + str(self._api.zone)
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return SUPPORT_FLAGS
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        return TEMP_UNITS[self._api.temperature_units]
+
+    @property
+    def min_temp(self):
+        """Return the minimum temperature."""
+        if self._min_temp is not None:
+            return self._min_temp
+        return super().min_temp
+
+    @property
+    def max_temp(self):
+        """Return the maximum temperature."""
+        if self._max_temp is not None:
+            return self._max_temp
+        return super().max_temp
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        if self._api.op_mode == 1 or self._api.op_mode == 4:
+            return min(self._api.set_points)
+        if self._api.op_mode == 2:
+            return max(self._api.set_points)
+        return None
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        return self._api.current_temperature
+
+    @property
+    def target_temperature_high(self):
+        """Return the highbound target temperature we try to reach."""
+        if self._api.op_mode == 3:
+            return max(self._api.set_points)
+        return None
+
+    @property
+    def target_temperature_low(self):
+        """Return the lowbound target temperature we try to reach."""
+        if self._api.op_mode == 3:
+            return min(self._api.set_points)
+        return None
+
+    @property
+    def current_humidity(self):
+        """Return the current humidity."""
+        return self._api.current_humidity
+
+    @property
+    def hvac_mode(self):
+        """Return the current hvac operation mode."""
+        if self._api.op_mode == 4:
+            return HVACMode.HEAT
+        return HVAC_MODES[self._api.op_mode]
+
+    @property
+    def hvac_modes(self):
+        """Return the list of available hvac operation modes."""
+        return HVAC_MODES
+
+    @property
+    def hvac_action(self):
+        """Return the current hvac state/action."""
+        if self._api.state == 3:
+            return HVACAction.IDLE
+        return HVAC_ACTIONS[self._api.state]
+
+    @property
+    def preset_mode(self):
+        """Return the current preset mode."""
+        if self._api.away_mode == 1:
+            return PRESET_AWAY
+        return None
+
+    @property
+    def preset_modes(self):
+        """Return a list of available preset modes."""
+        return [PRESET_NONE, PRESET_AWAY]
+
+    @property
+    def is_away_mode_on(self):
+        """Return the current away mode status."""
+        return self._api.away_mode
+
+    @property
+    def is_aux_heat(self):
+        """Return the current away mode status."""
+        if self._api.op_mode == 4:
+            return True
+        return False
+
+    @property
+    def fan_mode(self):
+        """Return the current fan mode."""
+        return FAN_MODES[self._api.fan_mode]
+
+    @property
+    def fan_modes(self):
+        """Return the list of available fan modes."""
+        return FAN_MODES
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature. API expects a tuple."""
+        if not self._api.away_mode:
+            if ((kwargs.get(ATTR_TEMPERATURE) is not None) and (
+                self._api.op_mode != 3)):
+                self._api.set_points = (kwargs.get(ATTR_TEMPERATURE), )
+            else:
+                self._api.set_points = (kwargs.get(ATTR_TARGET_TEMP_LOW),
+                                        kwargs.get(ATTR_TARGET_TEMP_HIGH))
+
+    def set_fan_mode(self, fan_mode):
+        """Set new fan mode."""
+        if not self._api.away_mode:
+            self._api.fan_mode = FAN_MODES.index(fan_mode)
+
+    def set_hvac_mode(self, hvac_mode):
+        """Set new hvac operation mode."""
+        if not self._api.away_mode:
+            self._api.op_mode = HVAC_MODES.index(hvac_mode)
+
+    def set_preset_mode(self, preset_mode):
+        """Set new preset mode."""
+        if preset_mode == PRESET_AWAY:
+            self._turn_away_mode_on()
+        else:
+            self._turn_away_mode_off()
+
+    def _turn_away_mode_on(self):
+        """Turn away mode on."""
+        self._api.away_mode = 1
+
+    def _turn_away_mode_off(self):
+        """Turn away mode off."""
+        self._api.away_mode = 0
+
+    def turn_aux_heat_on(self):
+        """Turn auxiliary heater on."""
+        self._api.op_mode = 4
+
+    def turn_aux_heat_off(self):
+        """Turn auxiliary heater off."""
+        self.set_hvac_mode(HVACMode.HEAT)
+
